e15cb383ffebb259c132bc88662926e3abf4696f
Michal Ingeli <mi@v3.sk>
2010-06-22 15:28:52 +0200

    Forgotten configuration fiel wit new option for commands not addressed by name

--- src/config.pm
+++ src/config.pm
@@ -1,51 +1,28 @@
 #!/usr/bin/env perl
 # NetBSD.sk bot configuration
-# $Id$
+# $Id: config.pm 35 2007-07-11 13:19:12Z lkundrak $
 
 use strict;
 
-@config::Ubiqs = ('Ubiq');
-@config::Revis = ('Revi');
+@config::Ubiqs = ();
+@config::Revis = ();
 $config::server = 'irc.upc.cz';
 $config::ircname = 'Robert Fico';
-@config::buddies = ('_8086');
+$config::prefix = "!";
+@config::buddies = ('_8086', "xyzz", "lkundrak", "marek", "ksyz");
 
 if ($config::degug = 0) {
 	@config::nicks = ('degug');
 	@config::channels = ('#testbed');
 } else {
-	@config::nicks = ('gerda', 'robik');
-	@config::channels = ('#netbsd.sk', '#include');
+	@config::nicks = ('Ubiq', 'Yane', 'Revi');
+	@config::channels = ('#netbsd.sk', '#fit', "#kanal");
 }
 
+# so we know our friends
 @config::ops = (
-	'~xyzz@147.175.55.175',
-	'~lkundrak@147.175.55.175',
-	'~pyxel@147.175.55.175',
-	'~e1m1@muff.zlo.sk',
-	'~fellow@stezka.nettel.cz',
-	'fatboy@195.168.3.218',
-	'xyzz@cray.x86.sk',
-	'^Ubiq@v0le.elvraba.edu.sk',
-	'ado@cloudlet.imladris.sk',
-	'crude@tarantula.valec.net',
-	'~mato@adsl.*.dsl.nextra.sk',
-	'~al.*@roa.*.dsl.club-internet.fr',
-	'~sine@158.195.99.101',
-	'~sine@213.151.251.83',
-	'waicak@.*vutbr.cz',
-	'~waico@mail.ui42.sk',
-	'norbert@vcielka.rec.uniba.sk',
-	'~fefo@84.16.39.226',
-	'.*@stip-static-48.213-81-186.telecom.sk',
-	'.*@norkia.kexik.net',
-	'.*sativa.morph.sk',
-	'.*@193.87.19.130',
-	'fellow@amee.nettel.cz',
-	'crg@lagoon.freebsd.lublin.pl',
-	'potion@morphium.synapsia.org',
-	'potion@student.tnuni.sk',
-	'xyzz@cray.x86.sk',
+	'.*@norkia.v3.sk',
+	'bon@127.0.0.1',
 );
 
 1;
